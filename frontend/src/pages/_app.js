c8a81a6f4d6f71701c01a1174054a58734d3fd9b
Kashyap Patel <kp2037723@gmail.com>
2025-06-19 10:50:10 +0530

    change -app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,11 +1,11 @@
+import { Provider } from "react-redux";
 import { store } from "@/config/redux/store";
 import "@/styles/globals.css";
-import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
-  return <>
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
-  </>;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
